9e0373b70b3fb0421db65e0ea230cda7c6814d41
nitya108 <54084557+nitya108@users.noreply.github.com>
2020-08-22 09:55:06 -0500

    Including changes for generate next state

--- Ruby Version/gameOfLife.rb
+++ Ruby Version/gameOfLife.rb
@@ -35,6 +35,57 @@ def printBoard(board,numRows,numCols)
   end
 end
 
+def liveNeighbours(board,i,j,numRows,numCols)
+  count = 0
+  directions = Array.new(8) { Array.new(2) }
+  directions[0] = [0, 1];
+  directions[1] = [1,0];
+  directions[2] = [1,1];
+  directions[3] = [1,-1];
+  directions[4] = [0,-1];
+  directions[5] = [-1,0];
+  directions[6] = [-1,-1];
+  directions[7] = [-1,1];
+  directions.each do |direction|
+    x, y = [i + direction[0], j + direction[1]];
+    if x.between?(0, numRows - 1) && y.between?(0, numCols - 1)
+      if board[x][y] == 1 || board[x][y] == 3
+        count += 1
+      end
+    end
+  end
+  count;
+end
+
+def generateNextStep(board,numRows, numCols)
+  for i in 0..numRows-1
+    for j in 0..numCols-1
+      countLiveCells = liveNeighbours(board,i,j,numRows,numCols);
+      if board[i][j] == 0
+        if countLiveCells == 3
+          board[i][j] = 2
+        end
+      else
+        if countLiveCells < 2 or countLiveCells > 3
+          board[i][j] = 3
+        end
+      end
+    end
+  end
+
+    for i in 0..numRows-1
+      for j in 0..numCols-1
+        if board[i][j] == 2
+          board[i][j] = 1
+        end
+       if board[i][j] == 3
+         board[i][j] = 0
+       end
+      end
+    end
+end
+
+
 puts "Welcome!\n";
 puts "Please enter the number of rows in the board: \n";
 numRows = gets.to_i;
